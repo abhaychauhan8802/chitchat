75ddf91202ef01caa0112e5c847b9561cb904925
abhaychauhan8802 <abhaychauhan8802@gmail.com>
2025-03-28 20:02:02 +0530

    make mongodb connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+// local imports
+import connectDB from "./lib/connectDB.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.get("/test", (req, res) => {
   res.send(`Server is running on port ${PORT}`);
 });
 
+app.use("/api/auth", authRouter);
+
 app.listen(PORT, () => {
   console.log("Server is listen on port 3000");
+  connectDB();
 });
